8a356219218fb263a6389414a088096eb2dcc58a
ramona-Saintandre <codinggrandma@gmail.com>
2021-01-03 21:47:29 -0500

    updated vars.tf file

---
+++ Terraform/vars.tf
@@ -0,0 +1,7 @@
+variable "prefix" {
+  description = "the prefi which should be used for all resources in this example"
+}
+variable "location"  { 
+    description = "the Azure Region in which all resources in this example shoudl be created. "
+  default = "East US"
+}
