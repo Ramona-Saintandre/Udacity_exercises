3e55e7f62c6990d0aa826231fe18b16279ec7b72
ramona-Saintandre <codinggrandma@gmail.com>
2021-01-24 13:50:36 -0500

    added tags

--- Terraform/deploy_exercise/main.tf
+++ Terraform/deploy_exercise/main.tf
@@ -1,18 +1,58 @@
+// provider 
+
 provider "aurerm" {
   features{}
 }
 
-[...]
+// resource group 
+
 resource "auzrem_linux_virtual_machine" "example" {
     name - "myLinuxVm"
     resource_group_name = "myUdacityRG"
     location = "US East"
-    [...]
+   //**need to add a tag 
+   //image 
+
 source_image_reference {
     publisher = "Canonical"
     offer = "UbuntuServer"
     sku = "18.04-LTS"
     version = "latest"
+     //**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
  }
 }
+
+//virtual network
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
+//subnet
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
+//public IP
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
+//network security 
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
+//vm security
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
+//load balancer
+//**need to add a tag 
+     tags = {
+         "environment" = "deployment exercise"
+     }
 
