fa054946e4191488889465d286aa4be17f7b2bb4
ramona-Saintandre <codinggrandma@gmail.com>
2021-01-24 22:25:31 -0500

    udated vars.tf in the resouredploy

--- Terraform/resource_deploytf/vars.tf
+++ Terraform/resource_deploytf/vars.tf
@@ -3,9 +3,10 @@
 
 
 variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-}
+  description = "The prefix which should be used for all rsources in this expample"
 
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
+  variable "location"{
+      description = "The Azure Region in which resources in thsi example should be created."
+      default = "East US"
+  }
 }
