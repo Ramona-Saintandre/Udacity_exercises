ef0cbead59debeab8db2979a33acd475cd6dfead
ramona-Saintandre <codinggrandma@gmail.com>
2021-01-24 22:18:39 -0500

    added public IP information from Terraform

--- Terraform/readme.md
+++ Terraform/readme.md
@@ -4,6 +4,9 @@
 [Intermediate Terraform conceptes](https://classroom.udacity.com/nanodegrees/nd082/parts/07bea1bd-9a45-4e1e-b79d-4a17b32e9faf/modules/7ddc5964-272c-4ffe-8353-a52f51a090cb/lessons/a1bde9e8-95f4-4686-8022-c5ed0395a23d/concepts/e276946f-bb82-4e89-a574-4aaf59a0c77a) 
 
 [terraform-provider-azurerm](https://github.com/terraform-providers/terraform-provider-azurerm/tree/master/examples/virtual-machines/linux/basic-password)
+
 [Hashicorp -  Azure Provider](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs)
 
 [Terraform documentation for Azure](https://classroom.udacity.com/nanodegrees/nd082/parts/07bea1bd-9a45-4e1e-b79d-4a17b32e9faf/modules/7ddc5964-272c-4ffe-8353-a52f51a090cb/lessons/a1bde9e8-95f4-4686-8022-c5ed0395a23d/concepts/1fac040d-188a-454d-ab20-8451c7be7561)
+
+[Create an Azure Virtual Machine with Terraform](https://www.codeproject.com/Articles/5260755/Create-an-Azure-Virtual-Machine-with-Terraform)
--- Terraform/resource_deploytf/main.tf
+++ Terraform/resource_deploytf/main.tf
@@ -1,13 +1,32 @@
 //resource_deploy with Terraform
 //https://classroom.udacity.com/nanodegrees/nd082/parts/07bea1bd-9a45-4e1e-b79d-4a17b32e9faf/modules/7ddc5964-272c-4ffe-8353-a52f51a090cb/lessons/a1bde9e8-95f4-4686-8022-c5ed0395a23d/concepts/12d4affb-564f-431f-aaca-0017678f8de7
 
+//[Create an Azure Virtual Machine with Terraform](https://www.codeproject.com/Articles/5260755/Create-an-Azure-Virtual-Machine-with-Terraform)
+
 provider "azurerm" {
   features {}
 }
 
+//resource group
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+   tags = {
+      "environment" = "lesson 4 terraform_deploy"
+  }
+}
+
+//Public IP address 
+
+resource "azurerm_public_ip" "main" {
+  name                = "acceptanceTestPublicIp1"
+ resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "lesson 4 terraform_deploy"
+  }
 }
 
 resource "azurerm_virtual_network" "main" {
@@ -15,15 +34,25 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+      "environment" = "lesson 4 terraform_deploy"
+  }
 }
 
+//subnet
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+   tags = {
+      "environment" = "lesson 4 terraform_deploy"
+  }
 }
 
+
+//network interface 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.main.name
@@ -33,14 +62,19 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+     tags = {
+      "environment" = "lesson 4 terraform_deploy"
+  }
   }
 }
 
+
+//virtual machine 
 resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F2"
+  size                            = "Standard_D2s v3"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
   disable_password_authentication = false
