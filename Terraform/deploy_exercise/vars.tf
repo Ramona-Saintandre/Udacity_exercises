79038e4f0365d8e352d0a69a619727a5fd586279
ramona-Saintandre <codinggrandma@gmail.com>
2021-01-24 22:30:16 -0500

    update prefix on all vars.tf files

--- Terraform/deploy_exercise/vars.tf
+++ Terraform/deploy_exercise/vars.tf
@@ -2,8 +2,7 @@ variable "prefix" {
   description = "The prefix which should be used for all rsources in this expample"
   default = "udacityL4"
 }
-
-  variable "location"{
-      description = "The Azure Region in which resources in thsi example should be created."
-      default = "East US"
-  }
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "East US"
+}
--- Terraform/resource_deploytf/vars.tf
+++ Terraform/resource_deploytf/vars.tf
@@ -4,9 +4,9 @@
 
 variable "prefix" {
   description = "The prefix which should be used for all rsources in this expample"
-
-  variable "location"{
-      description = "The Azure Region in which resources in thsi example should be created."
-      default = "East US"
-  }
+  default = "udacityL4"
+}
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "East US"
 }
